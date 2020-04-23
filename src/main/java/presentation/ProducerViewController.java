f732a9305a4553b6058c1aa3554dfdd7d2594eaf
Dannylyhn <dannylyhn@gmail.com>
2020-04-23 19:51:17 +0200

    Små layout changes
    LayoutChanges til producerView

--- src/main/java/presentation/ProducerViewController.java
+++ src/main/java/presentation/ProducerViewController.java
@@ -1,4 +1,24 @@
 package presentation;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ProducerViewController {
+
+    public void exitFromProducerPushed(ActionEvent Clicked) throws IOException {
+
+        Parent startViewParent = FXMLLoader.load(getClass().getResource("startView.fxml"));
+        Scene startViewScene = new Scene(startViewParent);
+        //This line gets the Stage information
+        Stage window = (Stage) ((Node) Clicked.getSource()).getScene().getWindow();
+        window.setTitle("TV2 - Applikation");
+        window.setScene(startViewScene);
+        window.show();
+    }
 }
